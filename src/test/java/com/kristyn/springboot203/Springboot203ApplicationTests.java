c217cf500eea00c54ad7275f9f22829fc0c9b3a6
kristynf <kristynf@bu.edu>
2020-02-17 16:52:32 -0500

    initial commmit

---
+++ src/test/java/com/kristyn/springboot203/Springboot203ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.springboot203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot203ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
